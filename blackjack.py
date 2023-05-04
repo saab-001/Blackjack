f0a80b7d301cb80416f82df7c6f7b532ee07755e
Sahib <sahibdeepsinghboparai053@gmail.com>
2023-05-04 00:32:46 -0400

    main

--- blackjack.py
+++ blackjack.py
@@ -10,10 +10,11 @@ logo = """
       |  \/ K|                            _/ |                
       `------'                           |__/           
 """
+
+
 def start():
-    
     start_select = input("Do you want to play Blackjack? Type 'y' or 'n': ").capitalize()
-    
+
     if start_select == "Y":
         print(logo)
     if start_select == "N":
@@ -22,22 +23,23 @@ def start():
         print("\nInvalid input!\n")
         start()
 
+
 start()
 
-def play(Colect_user,Colect_bot):
 
-    Cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+def play(Colect_user, Colect_bot):
+    Cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     Card_Drawn_user = [random.choice(Cards)]
     Card_Drawn_bot = [random.choice(Cards)]
     bot_points = sum(Colect_bot) + Card_Drawn_bot[0]
     user_points = sum(Colect_user) + Card_Drawn_user[0]
-    
-    if Card_Drawn_bot[0] == 11 and bot_points>10:
+
+    if Card_Drawn_bot[0] == 11 and bot_points > 10:
         Card_Drawn_bot[0] = 1
 
-    if Card_Drawn_user[0] == 11 and user_points>10:
+    if Card_Drawn_user[0] == 11 and user_points > 10:
         Card_Drawn_user[0] = 1
-    
+
     Colect_bot += Card_Drawn_bot
     Colect_user += Card_Drawn_user
     user_points = sum(Colect_user)
@@ -51,46 +53,47 @@ def play(Colect_user,Colect_bot):
         print("Computer's final hand: " + str(Colect_bot[0]) + "\t\t|\tFinal score: " + str(user_points))
         print("\nYou went over 21.\nYou Lose!!!")
         exit()
-        
+
     print("Your cards: " + str(Colect_user) + "\t|\tCurrent score: " + str(user_points))
     print("Computer's first card: " + str(Colect_bot[0]))
 
-    def Keep_playing(Colect_bot):
+    def Keep_playing(colect_bot):
         selection = input("type 'y' to deal or 'n' to pass: ").capitalize()
-        
+
         if selection == "Y":
-            play(Colect_user,Colect_bot)
+            play(Colect_user, colect_bot)
 
         if selection == "N":
             user_points = sum(Colect_user)
-            bot_points = sum(Colect_bot)  
+            bot_points = sum(colect_bot)
 
             while bot_points < 17:
-                Colect_bot += [random.choice(Cards)]
-                bot_points = sum(Colect_bot)
-              
+                colect_bot += [random.choice(Cards)]
+                bot_points = sum(colect_bot)
+
             if user_points < bot_points:
                 print("\nYour final hand: " + str(Colect_user) + "\t|\tFinal score: " + str(user_points))
-                print("Computer's final hand: " + str(Colect_bot[0]) + "\t|\tFinal score: " + str(bot_points))
+                print("Computer's final hand: " + str(colect_bot[0]) + "\t|\tFinal score: " + str(bot_points))
                 print("\n\tYou Lose!!!")
                 exit()
 
             if user_points == bot_points:
                 print("\nYour final hand: " + str(Colect_user) + "\t|\tFinal score: " + str(user_points))
-                print("Computer's final hand: " + str(Colect_bot[0]) + "\t|\tFinal score: " + str(bot_points))
+                print("Computer's final hand: " + str(colect_bot[0]) + "\t|\tFinal score: " + str(bot_points))
                 print("\n\tit's a Draw!!!")
                 exit()
 
             if user_points > bot_points:
                 print("\nYour final hand: " + str(Colect_user) + "\t|\tFinal score: " + str(user_points))
-                print("Computer's final hand: " + str(Colect_bot[0]) + "\t|\tFinal score: " + str(bot_points))
+                print("Computer's final hand: " + str(colect_bot[0]) + "\t|\tFinal score: " + str(bot_points))
                 print("\n\tYou Win!!!")
                 exit()
-            
+
         if selection != "Y" and selection != "N":
             print("\nInvalid input!\n")
-            Keep_playing(Colect_bot)
+            Keep_playing(colect_bot)
 
     Keep_playing(Colect_bot)
 
-play([],[])
+
+play([], [])
